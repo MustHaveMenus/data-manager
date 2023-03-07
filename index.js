96cd8d4477023b30a28171d235d4e29b81160289
Ionut Velicu <velicu.ionut@gmail.com>
2023-03-07 11:48:04 +0200

    fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import cors from "cors";
 import express from "express";
 import multipart from "connect-multiparty";
-import { deconstruct } from "./service.js";
+import { deconstruct, getPopMenu } from "./service.js";
 
 const app = express();
 const multipartMiddleware = multipart();
@@ -9,7 +9,7 @@ const multipartMiddleware = multipart();
 const port = 3000;
 
 const corsOptions = {
-  origin: ["http://localhost:8081/", "http://127.0.0.1:8081/"]
+  origin: ["http://localhost:8081/", "http://127.0.0.1:8081/", "https://test.mhmfun.com/", "https://www.musthavemenus.com/"]
 }
 
 app.use(express.json());
@@ -21,9 +21,14 @@ app.get("/status", (req, res) => {
   res.send(true);
 });
 
-app.post('/pdf/analyze', multipartMiddleware, async (req, res) => {
+app.post("/pdf/analyze", multipartMiddleware, async (req, res) => {
   res.header('Access-Control-Allow-Origin', "*");
   res.send(await deconstruct(req.files.file.path));
+});
+
+app.get("/pop/menu", async (req, res) => {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.send(await getPopMenu(req.query.path));
 });
 
 app.listen(port, () => {
--- service.js
+++ service.js
@@ -1,6 +1,15 @@
 import pdfjs from "pdfjs-dist";
+import puppeteer from "puppeteer";
+import jquery from "jquery";
+import {JSDOM} from "jsdom";
 
-const match = {g_d1_f1: "name", g_d1_f2: "description", g_d1_f5: "name", g_d1_f6: "description"};
+const BROWSER_HEADERS = { 
+  'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36', 
+  'upgrade-insecure-requests': '1', 
+  'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8', 
+  'accept-encoding': 'gzip, deflate, br', 
+  'accept-language': 'en-US,en;q=0.9,en;q=0.8' 
+}
 
 export async function deconstruct(file) {
   const matches = {};
@@ -26,4 +35,64 @@ export async function deconstruct(file) {
       type: ""
     }))
   };
+}
+
+export async function getPopMenu(url) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  
+  await page.setViewport({ width: 1280, height: 720 }); 
+  await page.setExtraHTTPHeaders(BROWSER_HEADERS); 
+  await page.goto(`https://${url}`);
+  await autoScrollPage(page);
+  await page.waitForTimeout(1000)
+  
+  const content = await page.content();
+  
+  await browser.close();
+
+  const dom = new JSDOM(content);
+  const $ = jquery(dom.window);
+  const $sections = $(".pm-menu-section");
+
+  const sections = [];
+  $sections.each(function() {
+    const $section = $(this);
+    const section = {
+      name: $section.find("h3").text(),
+      items: []
+    }
+
+    $section.find(".pm-next-dish-card-inner").each(function() {
+      const $item = $(this);
+      section.items.push({
+        name: $item.find("h4").text(),
+        description: $item.find("p span:last").text(),
+        price: $item.find(".pm-next-dish-price").text()
+      })
+    })
+
+    sections.push(section);
+  });
+
+  return sections;
+}
+
+async function autoScrollPage(page){
+  await page.evaluate(async () => {
+      await new Promise((resolve) => {
+          var totalHeight = 0;
+          var distance = 500;
+          var timer = setInterval(() => {
+              var scrollHeight = document.body.scrollHeight;
+              window.scrollBy(0, distance);
+              totalHeight += distance;
+
+              if(totalHeight >= scrollHeight - window.innerHeight){
+                  clearInterval(timer);
+                  resolve();
+              }
+          }, 100);
+      });
+  });
 }
