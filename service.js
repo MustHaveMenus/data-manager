f734d2e6639bbdd4578c77682d066f34ca30afad
Ionut Velicu <velicu.ionut@gmail.com>
2023-03-07 12:47:29 +0200

    fix

--- service.js
+++ service.js
@@ -38,7 +38,9 @@ export async function deconstruct(file) {
 }
 
 export async function getPopMenu(url) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
   const page = await browser.newPage();
   
   await page.setViewport({ width: 1280, height: 720 }); 
